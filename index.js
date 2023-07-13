479122837564e39861766d40a3b42d24d10b1123
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-07-13 18:26:05 +0300

    task done

--- index.js
+++ index.js
@@ -8,10 +8,26 @@ function createArticle (header, text) {
     const artHeader = document.createElement('h2');
     artHeader.innerText = header;
     const articleBody = document.createElement('p');
-    articleBody.immerText = text;
+    articleBody.innerText = text;
 
     artWrap.append(artHeader);
     artWrap.append(articleBody);
 
     return artWrap;
 }
+
+function printArticle(article) {
+    articlesWrap.appendChild(article);
+}
+
+
+// Достаём статьи из АПИ
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach(article => {
+        console.log(article);
+        printArticle(createArticle(article.title, article.body));
+    });
+  });
